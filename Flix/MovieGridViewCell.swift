3530c8de24e86764c0623879aa2c2c1436a95367
Trevor Rizz <trevorrizz@Trevors-Mac.local>
2020-10-03 19:14:28 -0700

    Unit 2

---
+++ Flix/MovieGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridViewCell.swift
+//  Flix
+//
+//  Created by Trevor Rizz on 10/3/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
